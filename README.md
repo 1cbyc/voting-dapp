b3c4697e103315e18308c9f9deed48781e3e1cf0
Isaac Emmanuel <isaacnsisong@gmail.com>
2025-05-25 23:26:23 +0100

    updated the voting contract properly

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # voting-dapp
 
-i had some errors 
+i had some errors when setting up ethers library. but i fixed it, i was mixing the nomic labs and nomics foundation plugins for ethers v5 and ethers v6, since it has been long i wrote this.
--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,34 +1,69 @@
-pragma solidity ^0.8.0;
+// pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 contract Voting {
-    struct Proposal {
+    struct Candidate {
         string name;
         uint voteCount;
     }
 
-    address public admin;
+    address public owner;
     mapping(address => bool) public hasVoted;
-    Proposal[] public proposals;
+    Candidate[] public candidates;
 
-    constructor(string[] memory proposalNames) {
-        admin = msg.sender;
-        for (uint i = 0; i < proposalNames.length; i++) {
-            proposals.push(Proposal({
-                name: proposalNames[i],
-                voteCount: 0
-            }));
+    constructor(string[] memory _candidateNames) {
+        owner = msg.sender;
+        for (uint i = 0; i < _candidateNames.length; i++) {
+            candidates.push(Candidate({ name: _candidateNames[i], voteCount: 0 }));
         }
     }
 
-    function vote(uint proposalIndex) public {
-        require(!hasVoted[msg.sender], "Already voted.");
-        require(proposalIndex < proposals.length, "Invalid proposal.");
+    function vote(uint candidateIndex) public {
+        require(!hasVoted[msg.sender], "You have already voted.");
+        require(candidateIndex < candidates.length, "Invalid candidate.");
 
         hasVoted[msg.sender] = true;
-        proposals[proposalIndex].voteCount += 1;
+        candidates[candidateIndex].voteCount++;
     }
 
-    function getProposals() public view returns (Proposal[] memory) {
-        return proposals;
+    function getCandidates() public view returns (Candidate[] memory) {
+        return candidates;
+    }
+
+    function getCanddidateCount() public view returns (uint) {
+        return candidates.length;
     }
 }
+
+// contract Voting {
+//     struct Proposal {
+//         string name;
+//         uint voteCount;
+//     }
+
+    // address public admin;
+    // mapping(address => bool) public hasVoted;
+    // Proposal[] public proposals;
+
+    // constructor(string[] memory proposalNames) {
+    //     admin = msg.sender;
+    //     for (uint i = 0; i < proposalNames.length; i++) {
+    //         proposals.push(Proposal({
+    //             name: proposalNames[i],
+    //             voteCount: 0
+    //         }));
+    //     }
+    // }
+
+//     function vote(uint proposalIndex) public {
+//         require(!hasVoted[msg.sender], "Already voted.");
+//         require(proposalIndex < proposals.length, "Invalid proposal.");
+
+//         hasVoted[msg.sender] = true;
+//         proposals[proposalIndex].voteCount += 1;
+//     }
+
+//     function getProposals() public view returns (Proposal[] memory) {
+//         return proposals;
+//     }
+// }
